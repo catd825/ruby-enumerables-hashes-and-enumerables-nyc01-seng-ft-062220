b2cf4fd46b7456b20b16eb6dcbaacc0acbab2f6e
Cathy D'Onofrio <catd825@gmail.com>
2020-05-31 17:03:31 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,9 +7,9 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
+  winner = "" #create new variable
+passengers.each do |suite, name| #iterate over passenger array - suite & name
+  if suite == :suite_a && name.start_with?("A") # if the suite is suite a and name starts with A, then that is the winner
     winner = name
   end
 end
